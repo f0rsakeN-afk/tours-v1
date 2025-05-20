83894ec592151766818a398ac887e29f7ad5b1ce
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-20 20:16:07 +0545

    added error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,14 +9,13 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-const sendErrorProd = (error, res) => {
+const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
   } else {
-    console.log("Error", err);
     res.status(500).json({
       status: "error",
       message: "Something went very wrong",
@@ -24,14 +23,28 @@ const sendErrorProd = (error, res) => {
   }
 };
 
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value"${value}. Please use another value`;
+  return new AppError(message, 400);
+};
 
+const handleJWTError = (err) =>
+  new AppError("Invalid token. Please log in again!", 401);
+
+const handleJWTExpireError = (err) =>
+  new AppError("Your token has expired! Please log in again", 401);
+
+module.exports = (err, req, res, next) => {
+  err.status = err.status || "error";
+  err.statusCode = err.statusCode || 500;
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+    let error = Object.create(err);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError(error);
+    if (error.name === "TokenExpiredError") error = handleJWTExpireError(error);
     sendErrorProd(error, res);
   }
 };
