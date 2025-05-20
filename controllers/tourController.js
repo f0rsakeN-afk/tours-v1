2720652ec92495b32e42c3e09240baa6117922ab
f0rsakeN-afk <nar3sh0@gmail.com>
2025-05-20 20:16:24 +0545

    added tourcontroller

---
+++ controllers/tourController.js
@@ -0,0 +1,69 @@
+const { useParams } = require("react-router-dom");
+const Tour = require("../models/tourModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const APIFeatures = require("../utils/APIFeatures");
+
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+exports.getTourById = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: newTour,
+    },
+  });
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  if (!tour) {
+    return next(new AppError("No tour found with this tour id", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError("No tour found with this id", 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
